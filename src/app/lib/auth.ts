4c32a05e40bd8231630bfbbabeed5a4f9d022db1
AviroopPaul <apavirooppaul10@gmail.com>
2025-09-07 20:00:07 +0530

    Refactor authentication setup by moving authOptions to a new lib/auth.ts file for better organization and maintainability.

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,40 @@
+import GithubProvider from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      authorization: {
+        params: {
+          scope: "repo read:org read:user user:email",
+        },
+      },
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
